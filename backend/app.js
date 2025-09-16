c83a799b406b2c4a4dcf6fa3407180d1d64937eb
Kwame Brempong <kwame@bvester.com>
2025-09-16 09:35:46 +0000

    Test auto-deployment: Update server reference and add comment

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
 // Elastic Beanstalk expects app.js or application.js as entry point
 // This file simply exports the server for EB compatibility
-module.exports = require('./server-aws.js');
+// Auto-deployment via AWS CodePipeline is now configured!
+module.exports = require('./production-server.js');
